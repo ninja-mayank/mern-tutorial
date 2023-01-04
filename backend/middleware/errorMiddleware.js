149bdbf86ec875dfc6c989dbdfecd1a783fb7cb5
ninja-mayank <mayanksood@Mayanks-MacBook-Air.local>
2023-01-04 15:59:52 +0530

    Goal controller and routes setup

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,13 @@
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500
+    res.status(statusCode)
+
+    res.json({
+        message : err.message,
+        stack : process.env.node_env == 'production' ? null : err.stack,
+    })
+}
+
+module.exports = {
+    errorHandler,
+}
